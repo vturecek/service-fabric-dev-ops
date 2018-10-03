3642010a482c8539c3b26996f3d2b3d14631b6f5
Vaclav Turecek <vturecek@microsoft.com>
2018-10-03 15:37:22 -0700

    Updating deploy script and template.

--- deploy/Scripts/Deploy.ps1
+++ deploy/Scripts/Deploy.ps1
@@ -1,26 +1,32 @@
 ﻿
 #!
 # use your repo absolute path here and run this command before continuing. PowerShell hates relative paths.
-$repoDir = "C:\path\to\netconf2018demo"
-$resourceGroupName = "<resource group name>"
+$repoDir = "C:\path\to\service-fabric-dev-ops"
+$resourceGroupName = ""
+$keyvualtName = ""
+$subscriptionName = ""
 $region = "westus2"
 
 # Login and subsription stuff
 Login-AzureRmAccount
-Set-AzureRmContext -Subscription "<your subscription name>"
+Set-AzureRmContext -Subscription $subscriptionName
+$subscription = Get-AzureRmSubscription -SubscriptionName $subscriptionName
 
 # Resource group
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $region
 
 # create a keyvault and upload a certificate to it
-Import-Module "$repoDir\ARM\Scripts\ServiceFabricRPHelpers\ServiceFabricRPHelpers.psm1"
+Import-Module "$repoDir\deploy\Scripts\ServiceFabricRPHelpers\ServiceFabricRPHelpers.psm1"
 
 # New cert
-$keyvault = Invoke-AddCertToKeyVault -SubscriptionId "<subscription GUID>" -ResourceGroupName $resourceGroupName -Location $region -VaultName '<keyvault name>' -CertificateName 'clustercert' -CreateSelfSignedCertificate -OutputPath "$repoDir\ARM\Certs" -DnsName 'mycluster.io' -Password "<password>"
+$keyvault = Invoke-AddCertToKeyVault -SubscriptionId $subscription.Id -ResourceGroupName $resourceGroupName -Location $region -VaultName $keyvualtName -CertificateName 'clustercert' -CreateSelfSignedCertificate -OutputPath "$repoDir\deploy\Certs" -DnsName 'mycluster.io' -Password ""
 
 # or existing cert
-$keyvault = Invoke-AddCertToKeyVault -SubscriptionId "<subscription GUID>" -ResourceGroupName $resourceGroupName -Location $region -VaultName '<keyvault name>' -CertificateName 'clustercert' -UseExistingCertificate -ExistingPfxFilePath "$repoDir\ARM\Certs\clustercert.pfx" -Password "<password>"
+$keyvault = Invoke-AddCertToKeyVault -SubscriptionId $subscription.Id -ResourceGroupName $resourceGroupName -Location $region -VaultName $keyvualtName -CertificateName 'clustercert' -UseExistingCertificate -ExistingPfxFilePath "$repoDir\deploy\Certs\clustercert.pfx" -Password ""
 
 # template test and deploy
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$repoDir\ARM\Templates\cluster-deploy.json" -TemplateParameterFile "$repoDir\ARM\Templates\cluster-deploy.parameters.json" -Verbose -clusterCertificateThumbprint $keyvault.CertificateThumbprint -sourceVaultValue $keyvault.SourceVault -clusterCertificateUrlValue $keyvault.CertificateURL -Debug
-New-AzureRmResourceGroupDeployment  -ResourceGroupName $resourceGroupName -TemplateFile "$repoDir\ARM\Templates\cluster-deploy.json" -TemplateParameterFile "$repoDir\ARM\Templates\cluster-deploy.parameters.json" -Verbose -clusterCertificateThumbprint $keyvault.CertificateThumbprint -sourceVaultValue $keyvault.SourceVault -clusterCertificateUrlValue $keyvault.CertificateURL
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$repoDir\deploy\Templates\cluster-deploy.json" -TemplateParameterFile "$repoDir\deploy\Templates\cluster-deploy.parameters.json" -Verbose -clusterCertificateThumbprint $keyvault.CertificateThumbprint -sourceVaultValue $keyvault.SourceVault -clusterCertificateUrlValue $keyvault.CertificateURL -location $region -Debug
+New-AzureRmResourceGroupDeployment  -ResourceGroupName $resourceGroupName -TemplateFile "$repoDir\deploy\Templates\cluster-deploy.json" -TemplateParameterFile "$repoDir\deploy\Templates\cluster-deploy.parameters.json" -Verbose -clusterCertificateThumbprint $keyvault.CertificateThumbprint -sourceVaultValue $keyvault.SourceVault -clusterCertificateUrlValue $keyvault.CertificateURL -location $region
+
+# get base-64 cluster certificate for DevOps
+[System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$repoDir\deploy\Certs\clustercert.pfx"))
