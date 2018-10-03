eda9b517888d9ba46d86ebeeebb095bd6db0155f
Vaclav Turecek <vturecek@microsoft.com>
2018-10-03 15:51:05 -0700

    Removing default services.

---
+++ src/Native/Voting/Scripts/Start-Service.ps1
@@ -0,0 +1,13 @@
+﻿$votingweb = Get-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingWeb -ServiceTypeName VotingWebType
+$votingdata = Get-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingData -ServiceTypeName VotingDataType
+
+
+if (!$votingweb)
+{
+    New-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingWeb -ServiceTypeName VotingWebType -Stateless -PartitionSchemeSingleton -InstanceCount -1 -ServicePackageActivationMode ExclusiveProcess
+}
+
+if (!$votingdata)
+{
+    New-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingData -ServiceTypeName VotingDataType -Stateful -PartitionSchemeUniformInt64 -LowKey 0 -HighKey 25 -PartitionCount 1 -HasPersistedState -MinReplicaSetSize 3 -TargetReplicaSetSize 3 -ServicePackageActivationMode ExclusiveProcess
+}
