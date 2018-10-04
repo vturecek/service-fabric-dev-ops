28ea954e8686e81b6a997bfed6e987e20c6273db
Vaclav Turecek <vturecek@microsoft.com>
2018-10-04 11:43:50 -0700

    Removing type from get-servicefabricservice

--- src/Native/Voting/Scripts/Start-Service.ps1
+++ src/Native/Voting/Scripts/Start-Service.ps1
@@ -1,10 +1,10 @@
-﻿$votingweb = Get-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingWeb -ServiceTypeName VotingWebType
-$votingdata = Get-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingData -ServiceTypeName VotingDataType
+﻿$votingweb = Get-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingWeb
+$votingdata = Get-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingData
 
 
 if (!$votingweb)
 {
-    New-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingWeb -ServiceTypeName VotingWebType -Stateless -PartitionSchemeSingleton -InstanceCount -1 -ServicePackageActivationMode ExclusiveProcess
+    New-ServiceFabricService -ApplicationName fabric:/Voting -ServiceName fabric:/Voting/VotingWeb -ServiceTypeName VotingWebType -Stateless -PartitionSchemeSingleton -InstanceCount 1 -ServicePackageActivationMode ExclusiveProcess
 }
 
 if (!$votingdata)
